e453b9508781630b6f45304701b3a0ae23522ea6
Gustavo Rafael Kühl <gustavo-kuhl@hotmail.com>
2022-10-04 12:10:43 -0300

    refactor: check if the argument was passed

--- dev-upgrade.sh
+++ dev-upgrade.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
 set -ex
 docker buildx build --load -t ghcr.io/elek/docker-storj-extension:1.0.0-nightly-$1 .
 docker extension update ghcr.io/elek/docker-storj-extension:1.0.0-nightly-$1
-
-
